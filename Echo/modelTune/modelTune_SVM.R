d1b6daaafdf25059f1a501aae6dc54d90a70f750
EchoYueXiong <50625126+EchoYueXiong@users.noreply.github.com>
2020-04-21 17:21:22 +0200

    Model Tune correction

--- Echo/modelTune/modelTune_SVM.R
+++ Echo/modelTune/modelTune_SVM.R
@@ -15,7 +15,7 @@ library(dataPreparation)
 
 ## get the task of different datasets with model tuning
 # import data
-completeData <- read.csv("F:/1-Credit_Card_Approval_Prediction/i2ml_final_project/credit_card_prediction/oh_data/mice_oh_data.csv")
+completeData <- read.csv2("F:/1-Credit_Card_Approval_Prediction/i2ml_final_project/credit_card_prediction/iv_data/dl_iv_data.csv")
 
 # transform the target y into factor type accordingly
 completeData$y <- as.factor(completeData$y)
@@ -34,7 +34,7 @@ colnames(train) <- make.names(colnames(train),unique = T)
 # show only warning messages
 lgr::get_logger("mlr3")$set_threshold("warn")
 
-# choose a specific model(RF) and set the parameters
+# choose a specific model(SVM) and set the parameters
 task <- TaskClassif$new(
   id="card_train", backend = train,
   target = "y"
@@ -44,35 +44,36 @@ task <- TaskClassif$new(
 # we chose 3 main parameters to tune the model
 # check available parameters
 set.seed(2020)
-learnerRF <-lrn("classif.ranger", predict_type = "prob")
+learnerSVM <-lrn("classif.svm", predict_type = "prob")
 
 # check for lower and upper bands for the three parameters
-learnerRF$param_set
+learnerSVM$param_set
 
-resamplingRF <- rsmp("cv", folds = 5)
-measuresRF <- msrs(c("classif.auc"))
+resamplingSVM <- rsmp("cv", folds = 5)
+measuresSVM <- msrs(c("classif.auc"))
 
 # 1--make parameter set
-tuneRF_ps <- ParamSet$new(list(
-  ParamInt$new("num.trees", lower = 1, upper = 500),
-  ParamInt$new("mtry", lower = 2, upper = 10),
-  ParamInt$new("min.node.size", lower = 10, upper = 50)
+tuneSVM_ps <- ParamSet$new(list(
+  ParamFct$new("kernel", levels = "polynomial"),
+  ParamInt$new("gamma", lower = 5, upper = 20)
 ))
 
-# 2--set terminator Eval/Combo/ClockTime/PerfReach/Stagnation
-terminatorRF <- term("evals", n_evals = 100)
+## levels = c("linear", "polynomial", "radical", "sigmoid")),
+
+# 2--set terminator Eval/Combo/ClockTime/PeSVMReach/Stagnation
+terminatorSVM <- term("evals", n_evals = 5)
 
 # 3--choose random search/ grid search
-tunerSearchRF <- tnr("random_search")
+tunerSearchSVM <- tnr("random_search")
 
 # make autoTuner
 at <- AutoTuner$new(
-  learner = learnerRF,
-  resampling = resamplingRF,
-  measures = measuresRF,
-  tune_ps = tuneRF_ps,
-  terminator = terminatorRF,
-  tuner = tunerSearchRF
+  learner = learnerSVM,
+  resampling = resamplingSVM,
+  measures = measuresSVM,
+  tune_ps = tuneSVM_ps,
+  terminator = terminatorSVM,
+  tuner = tunerSearchSVM
 )
 # record time
 classiftime1 <- proc.time()
